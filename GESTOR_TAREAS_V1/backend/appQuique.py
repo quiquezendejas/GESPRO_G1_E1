e22e6c6e0ee7f55975e90bd9582b44a6467ddddd
Tulio Aceves <A01711002@tec.mx>
2026-01-27 12:28:02 +0100

    #13 Se creó el spirnt 2, donde se ejecuta el frontend con un modelo las tareas

--- GESTOR_TAREAS_V1/backend/appQuique.py
+++ GESTOR_TAREAS_V1/backend/appQuique.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify
-from flask_cors import CORS # Importante para que el frontend pueda conectar
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) # Permite peticiones desde el frontend
+CORS(app)
+
+# Simulación de BD
+tasks = []
 
 @app.route('/health', methods=['GET'])
 def health_check():
@@ -10,6 +13,19 @@ def health_check():
         "status": "online",
         "message": "Backend funcionando correctamente"
     }), 200
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks), 200
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.json
+    task = {
+        "name": data.get("name")
+    }
+    tasks.append(task)
+    return jsonify(task), 201
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
