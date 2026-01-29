422304057486c811893f3e09d35787ca057ede66
Diego Enrique Zendejas Salazar <58782730+quiquezendejas@users.noreply.github.com>
2026-01-29 09:20:04 +0100

    actualización backend

--- GESTOR_TAREAS_V1/backend/appQuique.py
+++ GESTOR_TAREAS_V1/backend/appQuique.py
@@ -1,8 +1,14 @@
-from flask import Flask, jsonify
-from flask_cors import CORS  # <--- 1. Importar esto
+from flask import Flask, jsonify, render_template # Añadimos render_template
+from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app) # <--- 2. Activar esto para todas las rutas
+# Indicamos a Flask que los HTML están en la carpeta 'frontend'
+app = Flask(__name__, template_folder='../../frontend') 
+CORS(app)
+
+@app.route('/')
+def index():
+    # Esta función busca el archivo en la carpeta 'frontend' y lo muestra al usuario
+    return render_template('indexQuique.html')
 
 @app.route('/health', methods=['GET'])
 def health_check():
